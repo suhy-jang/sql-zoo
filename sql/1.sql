386788bdd3b4f11b5c97112271f327b24b6eda87
shjang7 <lucy.sh.jang@gmail.com>
2019-06-14 11:45:44 +0900

    Add spaces

--- sql/1.sql
+++ sql/1.sql
@@ -1,50 +1,63 @@
+# 1
 SELECT name, continent, population
   FROM world
 
+# 2
 SELECT name
   FROM world
  WHERE population >= 200000000
 
+# 3
 SELECT name, GDP/population
   FROM world
  WHERE population >= 200000000
 
+# 4
 SELECT name, population/1000000
   FROM world
  WHERE continent = 'South America'
 
+# 5
 SELECT name, population
   FROM world
  WHERE name IN ('France', 'Germany', 'Italy');
 
+# 6
 SELECT name
   FROM world
  WHERE name LIKE '%United%'
 
+# 7
 SELECT name, population, area
   FROM world
  WHERE area > 3000000 OR population > 250000000
 
+# 8
 SELECT name, population, area
   FROM world
  WHERE area > 3000000 XOR population > 250000000
 
+# 9
 SELECT name, ROUND(population/1000000.0,2), ROUND(GDP/1000000000.0,2)
   FROM world
  WHERE continent = 'South America'
 
+# 10
 SELECT name, ROUND(gdp/population, -3) AS per_capita_GDP
   FROM world
  WHERE gdp >= 1000000000000
 
+# 11
 SELECT name, capital
   FROM world
  WHERE LENGTH(name) = LENGTH(capital)
 
+# 12
 SELECT name, capital
   FROM world
  WHERE LEFT(name,1) = LEFT(capital,1) AND name <> capital
 
+# 13
 SELECT name
   FROM world
  WHERE name LIKE '%a%'
@@ -55,10 +68,10 @@ SELECT name
    AND name NOT LIKE '% %'
 
 /* Quiz */
-#1 5
-#2 4
-#3 2
-#4 4
-#5 2
-#6 4
-#7 3
+# 1 5
+# 2 4
+# 3 2
+# 4 4
+# 5 2
+# 6 4
+# 7 3
